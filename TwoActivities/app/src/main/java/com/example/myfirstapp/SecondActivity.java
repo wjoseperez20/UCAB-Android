c51466929eb47ab8b368003254538804ebdd9fec
Felix Morales <felix_morales_@hotmail.com>
2018-10-09 18:00:33 -0400

    first commit

---
+++ TwoActivities/app/src/main/java/com/example/myfirstapp/SecondActivity.java
@@ -0,0 +1,39 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView textMessage;
+    EditText textBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        this.Init();
+        this.LoadInformation();
+    }
+
+    private void Init(){
+        this.textMessage = findViewById(R.id.text_message);
+        this.textBack = findViewById(R.id.text_back);
+    }
+
+    private void LoadInformation(){
+        this.textMessage.setText(getIntent().getStringExtra("message"));
+    }
+
+    public void launchBack(View view) {
+        Intent intent = new Intent(this, Main.class);
+        intent.putExtra("reply",this.textBack.getText().toString());
+        this.textBack.setText(null);
+        startActivity(intent);
+    }
+}
